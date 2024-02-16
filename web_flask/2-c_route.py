fc95e11fef3155078ecc2af00b404a57eea5f369
mouadedd <mdx.edd@gmail.com>
2024-02-16 04:35:31 -0800

    display a message starting with c

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+web_app = Flask(__name__)
+
+
+@web_app.route('/', strict_slashes=False)
+def say_hello():
+    """ Function that sys Hello"""
+    return 'Hello HBNB!'
+
+
+@web_app.route('/hbnb')
+def hbnb():
+    """ Function that says hbnb """
+    return 'HBNB'
+
+
+@web_app.route('/c/<text>')
+def c_text(text):
+    """ Display C + message """
+    message = text.replace('_', ' ')
+    return f"C{message}"
+
+
+if __name__ == '__main__':
+    web_app.run(host='0.0.0.0', port=5000)
