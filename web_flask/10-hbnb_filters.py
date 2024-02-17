865ab14b953db5217e21ef4f7829018df607cad4
mouadedd <mdx.edd@gmail.com>
2024-02-17 06:51:40 -0800

    fun

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Starts a flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.amenity import Amenity
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def dispose(exception):
+    """ removecurrent SQLAlchemy session """
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', states=list(states.values()),
+                           amenities=list(amenities.values()))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
